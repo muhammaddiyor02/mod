f9d510e26a64ac3df8d95cf6d012c46a0fc9565a
muhammaddiyor02 <orifonov02@gmail.com>
2023-08-18 00:55:57 +0500

    register

--- js/main.js
+++ js/main.js
@@ -1,27 +1,30 @@
+var elModal = document.querySelector('.modal')
 var elInp1 = document.querySelector('.inp1')
 var elInp2 = document.querySelector('.inp2')
-var elBtn = document.querySelector('.btn')
-var elForm = document.querySelector('.form')
-var elMo = document.querySelector('.modal')
+var elSpan = document.querySelector('.span')
 var elBg = document.querySelector('.bgcolor')
+var elTit = document.querySelector('.tit')
 
 
 
-
-elForm.addEventListener('submit', function(){
-    if(elInp1.value == 'MUHAMMAD' && elInp2.value.length >= 8){
-        elMo.classList.add('modal_op')
-        elMo.classList.add('span')
-        elBg.classList.add('bgcolor_op')
-        elMo.textContent = 'passed'
+function fn(){
+    if(elInp1.value.length >= 7 && elInp2.value.length >= 8){
+        elInp1.classList.add('blue')
+        elInp2.classList.add('blue')
+        elInp1.classList.remove('danger')
+        elInp2.classList.remove('danger')
+        elModal.classList.toggle('modal_op')
+        elTit.textContent = 'SUCCESSFUL'
     }else{
-        elMo.classList.add('modal_op')
-        elMo.classList.add('span')
-        elBg.classList.add('bgcolor_op')
-        elMo.textContent = 'unpassed'
+        elInp1.classList.add('danger')
+        elInp2.classList.add('danger')
+        elInp1.classList.remove('blue')
+        elInp2.classList.remove('blue')
+        elModal.classList.toggle('modal_op')
+        elTit.textContent = 'UNSUCCESSFUL'
     }
+    
+}
+elSpan.addEventListener ('clcik',()=>{
+    elModal.classList.remove('mmm')
 })
-
-
-
-
